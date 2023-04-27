39ce8b66a981f5b4ed68e014d020eff0d6687c0c
Sauby <imsauby@gmail.com>
2023-04-27 14:04:31 +0530

    set up another branch 'this' for new updated website

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import "../styles/globals.css";
-import "../components/Layout";
-import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
-
-  return (
-    <Layout>
-      <Navbar />
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
