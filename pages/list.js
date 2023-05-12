a1be55547aeccb099bc10dd67bdd0d2aec1018da
Sauby <imsauby@gmail.com>
2023-05-12 21:34:38 +0530

    modified list page

--- pages/list.js
+++ pages/list.js
@@ -1,52 +1,105 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import Link from "next/link";
+import { PulseLoader } from "react-spinners";
+
 import styles from "../styles/List.module.css";
+import Image from "next/image";
 
 const List = () => {
+  const [search, setSearch] = useState("");
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleBookChange = (e) => {
+    setSearch(e.target.value);
+
+    // if (search.length >= 2) {
+    //   getBooks(search);
+    // } else {
+    //   setBooks([]);
+    // }
+  };
+
+  const getBooks = async (e) => {
+    e.preventDefault();
+    try {
+      if (search.length) {
+        const res = await fetch(`/api/getBook?book=${search}`);
+        const data = await res.json();
+        setBooks(data.items);
+        console.log(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    setLoading(false);
+  };
   return (
     <div>
       <Head>
         <title>2Book</title>
       </Head>
 
-      {/* Main Content here */}
       <main className={styles.main}>
-        <div>
+        {/* Search bar here */}
+        <form onSubmit={(e) => getBooks(e)}>
           <h1>Search</h1>
           <input
             type="text"
             className={styles.searchBar}
             placeholder="Type to search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
-        </div>
+          <button type="submit">Submit</button>
+        </form>
 
+        {/* Books here */}
         <div className={styles.books}>
-          <h1>Books</h1>
-          <hr />
-          <div className={styles.bookgrid}>
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
+          <div className={styles.bookHeader}>
+            <h1>Books</h1>
           </div>
+          <hr />
+          {loading ? (
+            <div className={styles.loaderBox}>
+              <PulseLoader speedMultiplier={0.5} color={"#f4a261"} />
+            </div>
+          ) : (
+            <div className={styles.bookgrid}>
+              {books.map((bk, i) => {
+                return <BookCard key={i} book={bk.volumeInfo} />;
+              })}
+            </div>
+          )}
         </div>
       </main>
     </div>
   );
 };
 
-export default List;
+// Card of books here
+const BookCard = ({ book }) => {
+  let photo = book.imageLinks.thumbnail;
 
-const BookCard = () => {
   return (
     <div className={styles.bookCard}>
-      <h2></h2>
+      <Image
+        src={photo}
+        alt={book.title}
+        className={styles.bookPhoto}
+        fill={true}
+      />
+      <div className={styles.details}>
+        <h4>{book.title}</h4>
+        <p>{book.subtitle}</p>
+      </div>
     </div>
   );
 };
+
+// export const getStaticProps = async () => {
+// }
+
+export default List;
